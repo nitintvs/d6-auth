4b0487f6717dfd9b2ee3f6dfc561888e5e156e86
nitintvs <nitin@theventurestudio.co>
2024-11-11 19:50:00 +0530

    d6 callback issue console

--- src/containers/AuthDialog/Callback.jsx
+++ src/containers/AuthDialog/Callback.jsx
@@ -6,16 +6,32 @@ const CallbackPage = () => {
   const auth = useAuth();
   const navigate = useNavigate();
 console.log("auth",auth)
-  useEffect(() => {
-    if (auth.isAuthenticated) {
-      // Navigate to home after successful login
-      navigate('/');
-    } else {
-      // Handle errors or redirect back to login if authentication fails
-      console.error('Authentication failed');
-      navigate('/');
-    }
-  }, [auth, navigate]);
+
+
+useEffect(() => {
+  if (auth && auth.isAuthenticated && auth.userData && auth.userData.access_token) {
+    // Store the token in local storage
+    localStorage.setItem("u-access-token", auth.userData.access_token);
+    console.log("Token saved to local storage:", auth.userData.access_token);
+    
+    // Navigate to a specific route
+    navigate('/');
+  } else {
+    //     // Handle errors or redirect back to login if authentication fails
+        console.error('Authentication failed');
+        navigate('/');
+      }
+}, [auth, navigate]);
+  // useEffect(() => {
+  //   if (auth.isAuthenticated) {
+  //     // Navigate to home after successful login
+  //     navigate('/');
+  //   } else {
+  //     // Handle errors or redirect back to login if authentication fails
+  //     console.error('Authentication failed');
+  //     navigate('/');
+  //   }
+  // }, [auth, navigate]);
 
   return <div>Processing login...</div>;
 };
