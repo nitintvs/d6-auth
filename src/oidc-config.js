721467db62b8fc31bf4a0766a4f80ca74f754edb
Nitintvs <nitin@theventurestudio.co>
2025-02-03 17:24:18 +0530

    mclardies domain mulitd6authurl

--- src/oidc-config.js
+++ src/oidc-config.js
@@ -27,17 +27,31 @@
 //     loadUserInfo: false, // Load additional user info from the userinfo endpoint
 //   };
 
+// export const oidcConfig = {
+//     authority: "https://id.zipalong.tech", // The authority URL (Issuer)
+//     clientId: "webbieshop-wt", // Your client ID
+//     // redirectUri: "https://oauthdebugger.com/debug", // Redirect URI after authentication
+//     redirectUri: "https://d6auth.vercel.app/login/callback", // Redirect URI after authentication
+//     responseType: "code", // Use Authorization Code flow
+//     scope: "openid profile", // Requested scopes
+//     silent_redirect_uri: "https://d6auth.vercel.app/login/callback",
+//     post_logout_redirect_uri: "https://d6auth.vercel.app/login",
+//     response_mode: "fragment",
+//     automaticSilentRenew: true, // Silent token renewal
+//     loadUserInfo: false, // Load additional user info from the userinfo endpoint
+//   };
 export const oidcConfig = {
     authority: "https://id.zipalong.tech", // The authority URL (Issuer)
     clientId: "webbieshop-wt", // Your client ID
     // redirectUri: "https://oauthdebugger.com/debug", // Redirect URI after authentication
-    redirectUri: "https://d6auth.vercel.app/login/callback", // Redirect URI after authentication
+    redirectUri: "https://multid6auth.vercel.app/login/callback", // Redirect URI after authentication
     responseType: "code", // Use Authorization Code flow
     scope: "openid profile", // Requested scopes
-    silent_redirect_uri: "https://d6auth.vercel.app/login/callback",
-    post_logout_redirect_uri: "https://d6auth.vercel.app/login",
+    silent_redirect_uri: "https://multid6auth.vercel.app/login/callback",
+    post_logout_redirect_uri: "https://multid6auth.vercel.app/login",
     response_mode: "fragment",
     automaticSilentRenew: true, // Silent token renewal
     loadUserInfo: false, // Load additional user info from the userinfo endpoint
   };
   
+  // https://multid6auth.vercel.app/
