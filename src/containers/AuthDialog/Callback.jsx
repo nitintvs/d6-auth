877d1bde24bc9449fbfce53f52dd8d1b05f03de4
Nitintvs <nitin@theventurestudio.co>
2025-02-04 15:02:22 +0530

    loggin user data from d6

--- src/containers/AuthDialog/Callback.jsx
+++ src/containers/AuthDialog/Callback.jsx
@@ -17,7 +17,7 @@ const CallbackPage = () => {
         try {
           // Store the token in local storage
           localStorage.setItem("D6-access-token", auth.userData.access_token);
-          localStorage.setItem("D6-userdata", auth);
+          localStorage.setItem("D6-userdata", JSON.stringify(auth));
           console.log("Token saved to local storage:", auth.userData);
            
           const userInfoResponse = await axiosInstance.post(
