a38e587e017f2f444b91c05539a894939756e26f
Nitintvs <nitin@theventurestudio.co>
2025-02-09 01:48:37 +0530

    fixed:countrylist issue in create and edit metrix

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ import { ColorProvider } from "utils/UIContext";
 import { LoaderProvider } from "Context/LoaderContext";
 import FullScreenLoader from "Context/FullScreenLoader";
 
-import { AuthProvider } from "oidc-react";
+import { AuthProvider } from "react-oidc-context";
 import { oidcConfig as initialOidcConfig } from "./oidc-config";
 import { isMobile } from "react-device-detect";
 import SilentRenewToken from "AuthWrapper";
