4e6f70fe9ed927ae95fbf215e96917d390fb7709
Nitintvs <nitin@theventurestudio.co>
2025-02-08 22:04:27 +0530

    fixed:countrylist issue in create and edit metrix

--- src/views/Landing/index.jsx
+++ src/views/Landing/index.jsx
@@ -168,7 +168,7 @@ const Landing = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        alert(`Host: ${window.location.host}\nURL: ${window.location.href}\nWebView: ${/WebView|wv/.test(navigator.userAgent)}`);
+        alert(`Host: ${window.location.host}${isMobile}\nURL: ${window.location.href}\nWebView: ${/WebView|wv/.test(navigator.userAgent)}`);
       }, []);
     function updateMetaDescription(newDescription) {
         const metaDescriptionTag = document.querySelector("meta[name='description']");
