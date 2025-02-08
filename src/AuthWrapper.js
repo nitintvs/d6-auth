ecc05adf00a37dd3aa3d9681cba896c3cd1b58e7
Nitintvs <nitin@theventurestudio.co>
2025-02-09 02:07:29 +0530

    fixed:countrylist issue in create and edit metrix

--- src/AuthWrapper.js
+++ src/AuthWrapper.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useAuth } from "oidc-react";
+import { useAuth } from "react-oidc-context";
 import axiosInstance from "configs/axiosConfig";
 import { APIRouteConstants } from "constants/routeConstants";
 
