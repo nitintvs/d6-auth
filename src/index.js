d0411f2611e8fae64b8329f4f959adfda6060cec
Nitintvs <nitin@theventurestudio.co>
2025-02-09 13:50:33 +0530

    fixed:countrylist issue in create and edit metrix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter, useNavigate } from "react-router-dom";
-import { SnackbarProvider } from "notistack";
-
+import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from 'notistack';
 import Routes from "routes/route";
 import store from "store/store";
 import { ColorProvider } from "utils/UIContext";
 import { LoaderProvider } from "Context/LoaderContext";
 import FullScreenLoader from "Context/FullScreenLoader";
-
 import { AuthProvider, useAuth } from "oidc-react";
 import { oidcConfig, createUserManager } from "./oidc-config";
 import { isMobile } from "react-device-detect";
@@ -26,41 +24,34 @@ const getParameterByName = (name, url) => {
 // Check for token_nonce in URL
 let tokenNonce = getParameterByName("token_nonce");
 
-// Clone and update the OIDC config with token_nonce if present
-const oidcConfig = { ...oidcConfig };
+// Update the OIDC config with token_nonce if present
+const finalOidcConfig = { ...oidcConfig };
 if (tokenNonce) {
-  oidcConfig.acr_values = `token_nonce:${tokenNonce}`;
+  finalOidcConfig.acr_values = `token_nonce:${tokenNonce}`;
   localStorage.setItem("token_nonce", tokenNonce);
-  console.log("Updated OIDC Config with token_nonce:", oidcConfig);
-}else{
-  tokenNonce= localStorage.getItem("token_nonce");
-  oidcConfig.acr_values = `token_nonce:${tokenNonce}`;
+} else if (localStorage.getItem("token_nonce")) {
+  tokenNonce = localStorage.getItem("token_nonce");
+  finalOidcConfig.acr_values = `token_nonce:${tokenNonce}`;
 }
 
-const userManager = createUserManager();
+const userManager = createUserManager(finalOidcConfig);
 
 // Determine which version of the app to render
 const isAuthApp = window?.location?.host === "multid6auth.vercel.app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render Auth App
 if (isAuthApp) {
-  // Render Auth-enabled App
-  alert(`auth app"tokenNonce",${tokenNonce}`)
-  alert(`window location",${window.location}`)
-  alert(`oidc config",${oidcConfig}`)
-  console.log("ismobile",isMobile)
+  console.log("ismobile", isMobile);
   root.render(
     <React.StrictMode>
-      <AuthProvider {...oidcConfig} userManager={userManager} autoSignIn={(tokenNonce||isMobile) ? true : false}>
-      <SilentRenewToken />
+      <AuthProvider {...finalOidcConfig} userManager={userManager} autoSignIn={(tokenNonce || isMobile) ? true : false}>
+        <SilentRenewToken />
         <Provider store={store}>
           <ColorProvider>
             <LoaderProvider>
-              <SnackbarProvider
-                autoHideDuration={3000}
-                maxSnack={3}
-                anchorOrigin={{ vertical: "top", horizontal: "right" }}
-              >
+              <SnackbarProvider autoHideDuration={3000} maxSnack={3} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
                 <BrowserRouter>
                   <FullScreenLoader />
                   <Routes />
@@ -76,24 +67,20 @@ if (isAuthApp) {
   // Render Regular App
   root.render(
     <React.StrictMode>
-       <AuthProvider {...oidcConfig} userManager={userManager} autoSignIn={false}>
-      <Provider store={store}>
-        <ColorProvider>
-          <LoaderProvider>
-            <SnackbarProvider
-              autoHideDuration={3000}
-              maxSnack={3}
-              anchorOrigin={{ vertical: "top", horizontal: "right" }}
-              >
-              <BrowserRouter>
-                <FullScreenLoader />
-                <Routes />
-              </BrowserRouter>
-            </SnackbarProvider>
-          </LoaderProvider>
-        </ColorProvider>
-      </Provider>
-              </AuthProvider>
+      <AuthProvider {...finalOidcConfig} userManager={userManager} autoSignIn={false}>
+        <Provider store={store}>
+          <ColorProvider>
+            <LoaderProvider>
+              <SnackbarProvider autoHideDuration={3000} maxSnack={3} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+                <BrowserRouter>
+                  <FullScreenLoader />
+                  <Routes />
+                </BrowserRouter>
+              </SnackbarProvider>
+            </LoaderProvider>
+          </ColorProvider>
+        </Provider>
+      </AuthProvider>
     </React.StrictMode>
   );
 }
